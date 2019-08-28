e81978bbb1ea31db9166240111f9a0b589949e19
tylerccaceres <tyler.caceres@gmail.com>
2019-08-28 11:59:21 -0400

    changes

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
-RSpec.feature "Add to cart", type: :feature, js: true do
-  scenario "They see all products" do
+RSpec.feature "login spec", type: :feature, js: true do
+  before :each do
+    User.create(
+      first_name: "first_name",
+      last_name: 'last_name',
+      email: 'email@email.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+  end
+  
+  scenario "They are able to signup " do
 
     visit '/signup'
 
@@ -21,6 +31,29 @@ RSpec.feature "Add to cart", type: :feature, js: true do
     page.find('.btn-primary').click
 
     expect(page).to have_text("Signed in as firstname lastname")
+    expect(page).to have_text("Logout")
+    expect(current_path).to eq("/")
+    save_screenshot
+  end
+
+  scenario "can login" do
+   
+
+    visit '/login'
+
+    save_screenshot
+
+    expect(page).to have_text("Login")
+    expect(page).to have_text("Signup")
+    # page.find('.btn-primary', match: :first).click
+    fill_in 'email', with: 'email@email.com'
+    fill_in 'password', with: 'password'
+    
+    save_screenshot
+
+    page.find_button('Submit').click
+
+    expect(page).to have_text("Signed in as first_name last_name")
     expect(page).to have_text("Logout")
     expect(current_path).to eq("/")
     save_screenshot
