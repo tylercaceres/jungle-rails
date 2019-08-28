04f856db8cc900a69a670a40a4ee9773aab5e0c9
tylerccaceres <tyler.caceres@gmail.com>
2019-08-28 14:00:03 -0400

    going to try to add cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  # before_filter :authorize
-
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: {case_sensitive: false}
